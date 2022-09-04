8b608be58de935150abc8ec71d34f9b47db43176
ahmadmhasann <ahmadmhasann@gmail.com>
2022-09-05 00:00:56 +0200

    add keypad and lcd drivers

---
+++ ECLIPSE_PROJECTS/AVR_DRIVERS/inc/Lcd.h
@@ -0,0 +1,52 @@
+/*
+ * Lcd.h
+ *
+ *  Created on: Sep 2, 2022
+ *      Author: ahmad
+ */
+
+#ifndef INC_LCD_H_
+#define INC_LCD_H_
+
+
+typedef enum {
+    LCD_DISPLAY_OFF,
+    LCD_DISPLAY_ON
+} Lcd_DisplayType;
+
+typedef enum {
+    LCD_CURSOR_OFF,
+    LCD_CURSOR_ON_BLINK,
+    LCD_CURSOR_ON_NOT_BLINK,
+} Lcd_CursorType;
+
+typedef enum {
+    LCD_ONE_LINE,
+    LCD_TWO_LINES
+} Lcd_LineType;
+
+typedef enum {
+    LCD_FONT_5X7,
+    LCD_FONT_5X10
+} Lcd_FontType;
+
+typedef struct 
+{
+    Lcd_DisplayType display_status;
+    Lcd_CursorType cursor_status;
+    Lcd_LineType line_status;
+    Lcd_FontType font_status;
+} Lcd_DisplayControlType;
+
+void Lcd_Init(void);
+void Lcd_DisplayCharacter (u8 character);
+void Lcd_DisplayString (u8* str);
+void Lcd_DisplayNumber (s32 num);
+void Lcd_DisplayClear(void);
+void Lcd_SetCursorPosition(u8 row, u8 col);
+void Lcd_DisplayShiftLeft(u8 counter);
+void Lcd_DisplayShiftRight(u8 counter);
+void Lcd_ReturnHome(void);
+void Lcd_DisplayControl(Lcd_DisplayControlType display);
+
+#endif /* INC_LCD_H_ */
---
+++ ECLIPSE_PROJECTS/AVR_DRIVERS/src/Lcd.c
@@ -0,0 +1,177 @@
+/*
+ * Lcd.c
+ *
+ *  Created on: Sep 2, 2022
+ *      Author: ahmad
+ */
+
+#include <std_types.h>
+#include <Macros.h>
+#include <Dio.h>
+#include <cpu_freq.h>
+#include <Lcd.h>
+#include <Lcd_cfg.h>
+
+static void Lcd_InitPins(void);
+static void Lcd_SendCommand(u8 command);
+static void Lcd_SendData(u8 data);
+
+void Lcd_Init(void)
+{
+    Lcd_InitPins();
+    _delay_ms(30);
+    Lcd_DisplayControlType display = {
+        LCD_DISPLAY_STATUS,
+        LCD_CURSOR_STATUS,
+        LCD_LINE_STATUS,
+        LCD_FONT_STATUS};
+    Lcd_DisplayControl(display);
+    Lcd_DisplayClear();
+}
+
+void Lcd_DisplayClear(void)
+{
+    Lcd_SendCommand(0b00000001);
+}
+
+void Lcd_DisplayCharacter(u8 character)
+{
+    Lcd_SendData(character);
+}
+
+void Lcd_DisplayString(u8 *str)
+{
+    while (*str != 0)
+    {
+        Lcd_SendData(*str);
+        str++;
+    }
+}
+void Lcd_SetCursorPosition(u8 row, u8 col)
+{
+    u8 address = col + (0x40 * row);
+    SET_BIT(address, 7);
+    Lcd_SendCommand(address);
+}
+
+void Lcd_DisplayControl(Lcd_DisplayControlType display)
+{
+    /* Set Lines and Font */
+    u8 command = 0b00110000;
+    if (display.line_status == LCD_TWO_LINES)
+    {
+        SET_BIT(command, 3);
+    }
+    if (display.font_status == LCD_FONT_5X10)
+    {
+        SET_BIT(command, 2);
+    }
+    Lcd_SendCommand(command);
+
+    /* Set Display and Cursor */
+    if (display.display_status == LCD_DISPLAY_OFF)
+    {
+        Lcd_SendCommand(0b00001000);
+    }
+    else if (display.display_status == LCD_DISPLAY_ON)
+    {
+        switch (display.cursor_status)
+        {
+        case LCD_CURSOR_OFF:
+            Lcd_SendCommand(0b00001100);
+            break;
+        case LCD_CURSOR_ON_BLINK:
+            Lcd_SendCommand(0b00001111);
+            break;
+        case LCD_CURSOR_ON_NOT_BLINK:
+            Lcd_SendCommand(0b00001110);
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void Lcd_DisplayNumber(s64 num)
+{
+    u64 reversed = 0;
+    u8 counter = 0;
+    if (num < 0)
+    {
+        Lcd_SendData('-');
+        num *= -1;
+    }
+    do
+    {
+        reversed = reversed * 10 + (num % 10);
+        num = num / 10;
+        counter++;
+    } while (num != 0);
+    do
+    {
+        Lcd_SendData(reversed % 10 + '0');
+        reversed = reversed / 10;
+        counter--;
+    } while (counter != 0);
+}
+
+void Lcd_DisplayShiftLeft(u8 counter)
+{
+}
+
+void Lcd_DisplayShiftRight(u8 counter)
+{
+}
+
+void Lcd_ReturnHome(void)
+{
+}
+
+static void Lcd_SendCommand(u8 command)
+{
+    Dio_SetPinLevel(LCD_PIN_RS, STD_LOW);
+    Dio_SetPinLevel(LCD_PIN_RW, STD_LOW);
+    Dio_SetPinLevel(LCD_PIN_D0, GET_BIT(command, 0));
+    Dio_SetPinLevel(LCD_PIN_D1, GET_BIT(command, 1));
+    Dio_SetPinLevel(LCD_PIN_D2, GET_BIT(command, 2));
+    Dio_SetPinLevel(LCD_PIN_D3, GET_BIT(command, 3));
+    Dio_SetPinLevel(LCD_PIN_D4, GET_BIT(command, 4));
+    Dio_SetPinLevel(LCD_PIN_D5, GET_BIT(command, 5));
+    Dio_SetPinLevel(LCD_PIN_D6, GET_BIT(command, 6));
+    Dio_SetPinLevel(LCD_PIN_D7, GET_BIT(command, 7));
+    Dio_SetPinLevel(LCD_PIN_EN, STD_HIGH);
+    _delay_ms(2);
+    Dio_SetPinLevel(LCD_PIN_EN, STD_LOW);
+}
+
+static void Lcd_SendData(u8 data)
+{
+    Dio_SetPinLevel(LCD_PIN_RS, STD_HIGH);
+    Dio_SetPinLevel(LCD_PIN_RW, STD_LOW);
+    Dio_SetPinLevel(LCD_PIN_D0, GET_BIT(data, 0));
+    Dio_SetPinLevel(LCD_PIN_D1, GET_BIT(data, 1));
+    Dio_SetPinLevel(LCD_PIN_D2, GET_BIT(data, 2));
+    Dio_SetPinLevel(LCD_PIN_D3, GET_BIT(data, 3));
+    Dio_SetPinLevel(LCD_PIN_D4, GET_BIT(data, 4));
+    Dio_SetPinLevel(LCD_PIN_D5, GET_BIT(data, 5));
+    Dio_SetPinLevel(LCD_PIN_D6, GET_BIT(data, 6));
+    Dio_SetPinLevel(LCD_PIN_D7, GET_BIT(data, 7));
+    Dio_SetPinLevel(LCD_PIN_EN, STD_HIGH);
+    _delay_ms(2);
+    Dio_SetPinLevel(LCD_PIN_EN, STD_LOW);
+}
+
+static void Lcd_InitPins(void)
+{
+    Dio_SetPinMode(LCD_PIN_RS, DIO_PIN_OUTPUT);
+    Dio_SetPinMode(LCD_PIN_RW, DIO_PIN_OUTPUT);
+    Dio_SetPinMode(LCD_PIN_EN, DIO_PIN_OUTPUT);
+    Dio_SetPinMode(LCD_PIN_D0, DIO_PIN_OUTPUT);
+    Dio_SetPinMode(LCD_PIN_D1, DIO_PIN_OUTPUT);
+    Dio_SetPinMode(LCD_PIN_D2, DIO_PIN_OUTPUT);
+    Dio_SetPinMode(LCD_PIN_D3, DIO_PIN_OUTPUT);
+    Dio_SetPinMode(LCD_PIN_D4, DIO_PIN_OUTPUT);
+    Dio_SetPinMode(LCD_PIN_D5, DIO_PIN_OUTPUT);
+    Dio_SetPinMode(LCD_PIN_D6, DIO_PIN_OUTPUT);
+    Dio_SetPinMode(LCD_PIN_D7, DIO_PIN_OUTPUT);
+}
