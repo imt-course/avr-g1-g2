364ab24f4b940ed2b2e7b99818f6060bbd561491
ahmadmhasann <ahmadmhasann@gmail.com>
2022-09-18 22:45:25 +0200

    Add LDR driver

--- ECLIPSE_PROJECTS/AVR_DRIVERS/inc/Ldr.h
+++ ECLIPSE_PROJECTS/AVR_DRIVERS/inc/Ldr.h
@@ -8,10 +8,11 @@
 #ifndef INC_LDR_H_
 #define INC_LDR_H_
 
-#define LDR_CHANNEL ADC_CHANNEL_ADC1
-#define LDR_RANGE_MIN 0
-#define LDR_RANGE_MAX 100
-#define LDR_MAX_MILLI_VOLTAGE 5000
+#define LDR_CHANNEL             ADC_CHANNEL_ADC1
+#define LDR_RANGE_MIN           0
+#define LDR_RANGE_MAX           100
+#define LDR_MAX_MILLI_VOLTAGE   5000
+
 #if LDR_RANGE_MAX < LDR_RANGE_MIN
 #error "LDR_RANGE_MAX should be greater than LDR_RANGE_MIN"
 #endif
@@ -19,8 +20,6 @@
 #error "LDR_MAX_MILLI_VOLTAGE should be greater than zero"
 #endif
 
-
 u16 Ldr_GetReading(void);
-
 
 #endif /* INC_LDR_H_ */
