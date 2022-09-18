0be38bca7bd757331cb973f2076ac8b198868a19
ahmadmhasann <ahmadmhasann@gmail.com>
2022-09-18 22:43:11 +0200

    Update Main Function

--- ECLIPSE_PROJECTS/AVR_DRIVERS/src/main.c
+++ ECLIPSE_PROJECTS/AVR_DRIVERS/src/main.c
@@ -16,21 +16,33 @@
 #include <Calculator.h>
 #include <Gie.h>
 #include <ExtInterrupt.h>
-
-
-void ToggleLed (void) {
-	Dio_FlipPinLevel(DIO_PORTD, DIO_PIN0);
-}
+#include <Adc.h>
+#include <Ldr.h>
+#include <Lm35.h>
 
 int main(void)
 {
-	Dio_SetPinMode(DIO_PORTD, DIO_PIN2, DIO_PIN_INPUT_PULLUP);	/* External Interrupt Pin Input Pullup */
-	Dio_SetPinMode(DIO_PORTD, DIO_PIN0, DIO_PIN_OUTPUT);		/* LED Pin Output */
-	ExtInterrupt_Init();
-	ExtInterrupt_EnableNotification(EXT_INTERRUPT_INT0, ToggleLed);
-	Gie_Enable();
-	while (1)
-	{
-
+	u16 result;
+	/* Set ADC Pins as Input */
+	Dio_SetPinMode(DIO_PORTA, DIO_PIN0, DIO_PIN_INPUT_FLOATING);
+	Dio_SetPinMode(DIO_PORTA, DIO_PIN1, DIO_PIN_INPUT_FLOATING);
+	/* Initialize ADC */
+	Adc_Init(&Adc_Cfg);
+	/* Initialize LCD */
+	Lcd_Init();
+	while(1) {
+		/* Reading Temperature */
+		result = Lm35_GetTemperature();
+		Lcd_DisplayClear();
+		Lcd_DisplayString("Temperature = ");
+		Lcd_DisplayNumber(result);
+		Lcd_DisplayString(" C");
+		/* Reading LDR */
+		result = Ldr_GetReading();
+		Lcd_SetCursorPosition(1,0);
+		Lcd_DisplayString("LDR = ");
+		Lcd_DisplayNumber(result);
+		Lcd_DisplayString(" mV");
+		_delay_ms(1000);
 	}
 }
