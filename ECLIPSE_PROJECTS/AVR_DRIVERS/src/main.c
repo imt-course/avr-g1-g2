e785d20c5af110814972dd860060ebc9371edf6b
ahmadmhasann <ahmadmhasann@gmail.com>
2022-10-03 16:57:49 +0200

    Add ICU SW and HW example in main.c

--- ECLIPSE_PROJECTS/AVR_DRIVERS/src/main.c
+++ ECLIPSE_PROJECTS/AVR_DRIVERS/src/main.c
@@ -22,10 +22,84 @@
 #include <Gpt.h>
 #include <Interrupts.h>
 #include <Pwm.h>
+#include <Icu.h>
+
+
+#define ICU_HW
+
+
+/******************************************************************
+ * Hardware ICU Example
+ *****************************************************************/
+#ifdef ICU_HW
+
+volatile u32 Time;
+volatile u32 Time_on;
+volatile u32 counter_timer = 0;
+
+void Handler_Icu(u16 value);
+void handler_Timer1_Ov(void);
+
+int main (void) {
+	Lcd_Init();
+	Icu_Init();
+	Gpt_Init();
+	Pwm_Init(PWM_CHANNEL_0);
+	Pwm_SetPrescaler(PWM_CHANNEL_0, PWM_PRESCALER_8);
+	Pwm_SetCompareValue(PWM_CHANNEL_0, 120);
+	Icu_SetEdge(ICU_EDGE_RISING);
+	Icu_EnableNotifications(Handler_Icu);
+	Gpt_EnableNotification(GPT_INT_SOURCE_TIM1_OV, handler_Timer1_Ov);
+	Gie_Enable();
+	while (1)
+	{
+		Lcd_DisplayClear();
+		Lcd_Print("Time = %d", Time);
+		Lcd_SetCursorPosition(1, 0);
+		Lcd_Print("Time On = %d", Time_on);
+		_delay_ms(1000);
+	}
+}
+
+void Handler_Icu(u16 value) {
+	static u8 counter = 1;
+	static u16 temp;
+	switch (counter)
+	{
+	case 1:
+		temp = value;
+		counter_timer = 0;
+		counter++;
+		break;
+	case 2:
+		Time = value + counter_timer*0xFFFF - temp;
+		counter++;
+		Icu_SetEdge(ICU_EDGE_FALLING);
+		break;
+	case 3:
+		Time_on = value + counter_timer*0xFFFF - Time - temp;
+		counter = 1;
+		Icu_SetEdge(ICU_EDGE_RISING);
+		break;
+	default:
+		break;
+	}
+}
+
+void handler_Timer1_Ov(void) {
+	counter_timer++;
+}
+
+#endif
+
+
+
+
 
 /******************************************************************
  * Software ICU Example
  *****************************************************************/
+#ifdef ICU_SW
 
 volatile u16 timer_counter = 0;
 volatile u16 Time;
@@ -55,11 +129,9 @@ void ICU_SW(void)
 	while (1)
 	{
 		Lcd_DisplayClear();
-		Lcd_DisplayString((u8*)"Time = ");
-		Lcd_DisplayNumber(Time);
+		Lcd_Print("Time = %d", Time);
 		Lcd_SetCursorPosition(1, 0);
-		Lcd_DisplayString((u8*)"Time On = ");
-		Lcd_DisplayNumber(Time_on);
+		Lcd_Print("Time On = %d", Time_on);
 		_delay_ms(1000);
 	}
 }
@@ -93,3 +165,5 @@ void Handler_ExtInt0(void)
 		break;
 	}
 }
+
+#endif
